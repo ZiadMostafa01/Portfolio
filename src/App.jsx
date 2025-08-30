37f82af0e21ec5a7077cb105a91028544a9b5bf8
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2025-08-30 20:53:55 +0300

    Update Scroll Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import BrandSlider from "./components/Marquee";
-// import HeroParticles from "./components/HeroParticles";
+import { useEffect, useState } from "react";
 
 function App() {
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 300 ? setshowScrollBtn(true) : setshowScrollBtn(false);
+    });
+  }, []);
+  const [showScrollBtn, setshowScrollBtn] = useState(false);
   return (
     <>
       {/* <Navbar /> */}
       <Hero />
       <About />
-      {/* <div className="h-[1px] bg-zinc-700"></div> */}
       <Projects />
-      {/* <div className="h-[1px] bg-zinc-700"></div> */}
       <Contact />
-      {/* <Footer /> */}
-      <a
-        href="#Home"
-        className="fixed bottom-5 right-5 w-10 h-10 shadow-lg flex justify-center items-center rounded-full border border-blue-500/20 text-blue-500"
-      >
-        <i className="fas fa-arrow-up"></i>
-      </a>
+
+        <a
+          href="#Home"
+          style={{opacity: showScrollBtn ? 1 : 0}}
+          className="fixed bottom-5 right-5 w-10 h-10 shadow-lg flex justify-center items-center rounded-full transition duration-700 border border-blue-500/20 text-blue-500"
+        >
+          <i className="fas fa-arrow-up"></i>
+        </a>
+   
     </>
   );
 }
